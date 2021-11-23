eb0c0a95f336eda155e76ae42fda666a6c27e086
GiovanniConti <giovanni.conti@live.it>
2021-11-23 19:58:35 +0100

    Added and completed section WorkWithUs

---
+++ src/components/WorkWithUs.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="workingWithUs">
+    <div class="textContainer">
+      <h2 class="my-3"> {{sectionTitle}} <strong class="fst-italic">a message!</strong></h2>
+      <p>{{sectionInfo}}</p>
+      <button class="btn">CONTACT</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'WorkingWithUs',
+  data(){
+    return{
+      sectionTitle: 'Want to work with us? send us ',
+      sectionInfo:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste voluptatibus, qui unde voluptates laudantium, maxime quam consequatur quibusdam veritatis beatae quaerat fugit ad accusamus est delectus nam natus officia atque?',
+    };
+  },
+}
+</script>
+
+<style lang='scss'>
+.workingWithUs{
+  background-color: rgb(250, 247, 242);
+  padding: 100px 0;
+
+  .textContainer{
+    max-width: 75%;
+    margin: auto;
+    
+    h2{
+      font-size: 60px;
+      font-weight: normal;
+    }
+    p{
+      font-size: 20px;
+      color: rgb(150, 150, 150);
+    }
+    button{
+      background-color: rgb(216, 240, 227);
+      padding: 15px 50px;
+    }
+  }
+}
+</style>
