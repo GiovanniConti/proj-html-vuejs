d9a509bd1ea703b286f88cd002d8f0eca35d803d
GiovanniConti <giovanni.conti@live.it>
2021-11-24 16:53:00 +0100

    Fixed Hero img

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,9 @@
 <template>
   <header class="m-4">
     <div class="navbar p-0">
+      <!-- logo Nav -->
       <img :src="require('@/assets/img/' + logoHeader)" alt="Logo Header" class="img-nav">
-
+      <!-- Nav List -->
       <ul class="navList list-inline m-0 fw-bold">
         <li
           class="list-inline-item mx-3 px-1"
@@ -16,18 +17,27 @@
           </li>
       </ul>
     </div>
+    <!-- Section HERO (Carosel) -->
     <div class="hero container-xl py-5 px-0">
       <div class="row py-5">
-        <div class="col hero-text text-start p-0">
+        <!-- Container Hero Text (L) -->
+        <div class="col hero-text text-start">
           <h2 class="hero-title m-0">Devotion that never <strong class="fst-italic">ends</strong> </h2>
           <p class="my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eligendi laboriosam quae, saepe ratione iure? Laboriosam quae, saepe ratione iure?</p>
           <button class="btn btn-hero my-3">READ MORE</button>
         </div>
+        <!-- Container Hero Img (R) -->
+        <div class="col hero-img">
+          <img src="@/assets/img/hero-img.jpg" alt="Slider-1" class="img-fluid">
+        </div>
+        <!--
         <div class="col hero-img d-flex p-0">
           <img src="@/assets/img/h-2-slider-img-15.png" alt="Slider-1">
           <img src="@/assets/img/h-2-slider-img-16.png" alt="Slider-2">
           <img src="@/assets/img/h-2-slider-img-17.png" alt="Slider-3" class="hero-img-flowers">
         </div>
+        -->
+        <!-- Carosel overlay elements -->
         <div class="container-chevrons-dots">
           <a href="#" class="chevronLink chevronLink-L"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>
           <a href="#" class="chevronLink chevronLink-R"><i class="fa fa-chevron-right" aria-hidden="true"></i></a>
@@ -104,13 +114,18 @@ header{
             .hero-title{
               font-size: 75px;
             }
+
+            p{
+              color: #878787;
+            }
             .btn-hero{
               font-size: 11px;
               font-weight: bold;
-              border: 2px solid rgb(225, 192, 176);
+              border: 2px solid #E1C0B0;
               padding: 15px 30px;
               border-radius: 0;
               box-shadow: none;
+              letter-spacing: 5px;
             }
       }
 
@@ -134,7 +149,7 @@ header{
           position: absolute;
           top: 50%;
           transform: translateY(-50%);
-          background-color: rgb(225, 192, 176);
+          background-color: #E1C0B0;
           color: white;
           width: 30px;
           height: 30px;
@@ -155,7 +170,7 @@ header{
         }
 
         .list-dots{
-          color: rgb(225, 192, 176);
+          color: #E1C0B0;
           list-style: none;
           position: absolute;
           bottom: 0;
